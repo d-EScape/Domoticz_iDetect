4fe39ad17b5bb1a3f269ff0d9a8f404a2df453a5
d-EScape <8693608+d-EScape@users.noreply.github.com>
2019-11-29 12:13:26 +0100

    Some documentation on how to create your own tracker class added to fake_tracker.py

--- trackers/fake_tracker.py
+++ trackers/fake_tracker.py
@@ -1,3 +1,25 @@
+# Example of a very simple tracker. It returns fake results and can also be used for testing.
+# The __init__() method should call the super()._init__() method with all the arguments, so the tracker_base
+# is properly created. The tracker_base will hold of all the (generic) variables for the tracker object, so you don't
+# have to.
+# The tracker_base also takes care of the polling interval and data parsing, so the router specific module can
+# be quite simple. It only needs a method for polling and return its data to the receiver_callback of
+# the tracker_base. The data can be a list (containing tag addresses - for now MAC or IP), but it can also be
+# a string. The receiver_callback will search for valid addresses in the (dirty) string.
+# Please do not modify tracker_base unless the change is an enhancement for every kind of
+# tracker. You can always expand upon the tracker_base by including one of its methods in your tracker class AND 
+# calling the super().<methodname> to execute both.
+#
+# self.is_ready variable has to be set to True by your own tracker class or it will never poll.
+#
+# If your tracker uses any kind of background thread/connection make sure you end/close it using a
+# stop_now(self) method and add super().stop_now() at the end. Domoticz will hang if you don't and try to stop the hardware!
+# See ssh_tracker for an example.
+# If you want to create a tracker that uses ssh you should base it on the ssh_tracker (which itself is again based on tracker_base)
+# See (amongst others) ssh_routeros.py for an example on how simple it can be.
+#
+# Last but not least: add your tracker class to __init__.py in the trackers directory, otherwise it will not be known to the plugin.
+
 import Domoticz
 from trackers.tracker_base import tracker
 
@@ -8,7 +30,12 @@ class fake_tracker(tracker):
 		Domoticz.Debug(self.tracker_ip + ' Is a FAKE tracker, will do a fake poll and return fake results')
 		
 	def poll_present_tag_ids(self):
+		# Add code to retrieve data from a tracker and return the (raw string of formatted list) data to
+		# the receiver_callback that is in the base_tracker
 		self.receiver_callback(['11:22:33:44:55:66', '10:20:30:40:50:60', 'Aa:bB:CC:DD:EE:0f'])
+		# This would also work:
+		#self.receiver_callback('i am a string with some 11:22:33:44:55:66 addrresses and other 10:20:30:40:50:60 unusable text that will be ignored Aa:bB:CC:DD:EE:0f addresses may be upper or lower case')
 	
 	def prepare_for_polling(self):
+		# This metod is not required but setting self.is_true is.
 		self.is_ready = True
