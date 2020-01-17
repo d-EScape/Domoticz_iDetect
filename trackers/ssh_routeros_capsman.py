4403df90e9e3e86a4825e247e156164ce635cdf2
d-EScape <8693608+d-EScape@users.noreply.github.com>
2020-01-17 17:36:09 +0100

    added routeros-capsman tracker type

---
+++ trackers/ssh_routeros_capsman.py
@@ -0,0 +1,11 @@
+from trackers.ssh_tracker import ssh_tracker
+import helpers.tracker_cli_helper as tracker_cli_helper
+
+class ssh_routeros_capsman(ssh_tracker):
+	def __init__(self, tracker_ip, tracker_port, tracker_user, tracker_password, tracker_keyfile, poll_interval):
+		super().__init__(tracker_ip, tracker_port, tracker_user, tracker_password, tracker_keyfile, poll_interval)
+		self.prepare_for_polling()
+
+	def prepare_for_polling(self):
+		self.trackerscript = "caps-man registration-table print"
+		self.is_ready = True
