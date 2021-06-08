daa6944fc49565bcba758a6ecb5fb6e7d248cd48
BabaIsYou <85300265+BabaIsYou@users.noreply.github.com>
2021-06-08 12:03:25 -0400

    Update ssh_aimesh_json.py

--- trackers/ssh_aimesh_json.py
+++ trackers/ssh_aimesh_json.py
@@ -5,7 +5,7 @@
 from trackers.ssh_tracker import ssh_tracker
 import helpers.tracker_cli_helper as tracker_cli_helper
 
-class ssh_brctl(ssh_tracker):
+class ssh_aimesh_json(ssh_tracker):
 	def __init__(self, tracker_ip, tracker_port, tracker_user, tracker_password, tracker_keyfile, poll_interval):
 		super().__init__(tracker_ip, tracker_port, tracker_user, tracker_password, tracker_keyfile, poll_interval)
 		self.prepare_for_polling()
