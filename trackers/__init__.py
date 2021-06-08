8efa69ac7627d74383488677be0a753f70b289f5
BabaIsYou <85300265+BabaIsYou@users.noreply.github.com>
2021-06-08 11:42:34 -0400

    Update __init__.py

--- trackers/__init__.py
+++ trackers/__init__.py
@@ -9,6 +9,7 @@ from trackers.ssh_routeros_capsman import ssh_routeros_capsman
 from trackers.ssh_routeros_arp import ssh_routeros_arp
 from trackers.ssh_zyxel_arp import ssh_zyxel_arp
 from trackers.ssh_unifi_usg_arp import ssh_unifi_usg_arp
+from trackers.ssh_aimesh_json import ssh_aimesh_json
 from trackers.http_orbi import http_orbi
 from trackers.http_unifi import http_unifi
 from trackers.http_omada import http_omada
@@ -23,6 +24,7 @@ poll_methods = {
 	'routeros-capsman': ssh_routeros_capsman,
     'zyxel-arp': ssh_zyxel_arp,
     'unifiusg-arp': ssh_unifi_usg_arp,
+	'aimesh_json' : ssh_aimesh_json,
     'unifi-http': http_unifi,
     'orbi-http': http_orbi,
     'omada-http': http_omada,
