76f906e924614487bbf74fa558431a121ec0d949
d-EScape <8693608+d-EScape@users.noreply.github.com>
2024-11-01 13:02:18 +0100

    Updated fritzbox tracker. Now using FritzHosts instead of FritzWLAN library module.

--- trackers/fritzbox.py
+++ trackers/fritzbox.py
@@ -1,18 +1,16 @@
-# Tracker for Fritzbox and tested on Fritzbox 7590
-# Needs fritzconnection by Klaus Bremer, so make sure you did: pip install fritzconnection
-# Should work on other models. The number of wlans's is automatically detected.
+# Tracker for Fritzbox and tested on Fritzbox 4060
+# Needs fritzconnection by Klaus Bremer et al, so make sure you did: pip install fritzconnection
 
 import DomoticzEx
 from trackers.tracker_base import tracker
 from fritzconnection import FritzConnection
-from fritzconnection.lib.fritzwlan import FritzWLAN
+from fritzconnection.lib.fritzhosts import FritzHosts
 from time import sleep
 
 class fritzbox(tracker):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.busy = False
-		self.wlans = []
 		self.prepare_for_polling()
 		
 	def poll_present_tag_ids(self):
@@ -20,40 +18,25 @@ class fritzbox(tracker):
 		listofhosts=[]
 		listofactivehosts=[]
 		try:
-			for wlan in self.wlans:
-				listofhosts = listofhosts + wlan.get_hosts_info()
+			listofhosts = self.fritzlist.get_hosts_info()
 		except Exception as e:
 			DomoticzEx.Error(self.tracker_ip + ' Fritzbox polling error: ' + str(e))
-		for host in listofhosts:
-			if host['status']==True:
-				listofactivehosts.append(host['mac'])
-		self.receiver_callback(listofactivehosts)
+			self.prepare_for_polling()
+		else:
+			for host in listofhosts:
+				if host['status']==True:
+					listofactivehosts.append(host['mac'])
+			self.receiver_callback(listofactivehosts)
 		self.busy = False
 		
 	def prepare_for_polling(self):
 		try:
-			self.session = FritzConnection(address=self.tracker_ip, password=self.tracker_password)
+			self.session = FritzConnection(address=self.tracker_ip, user=self.tracker_user, password=self.tracker_password)
+			self.fritzlist = FritzHosts(self.session)
 		except Exception as e:
 			DomoticzEx.Log('Fritzbox ' + self.tracker_ip + ' failed to connect. Check your setup and restart the plugin.')
 			DomoticzEx.Debug(e)
 		else:
-			servicenum=1
-			reachedend=False
-			while not reachedend:
-				newwlan = FritzWLAN(self.session, service=servicenum)
-				try:
-					ssid = newwlan.ssid
-				except Exception as e:
-					reachedend = True
-					DomoticzEx.Debug('Fritzbox does not have service ' + str(servicenum))
-					DomoticzEx.Debug(e)
-				else:
-					self.wlans.append(newwlan)
-					DomoticzEx.Debug('Fritzbox has service ' + str(servicenum) + ' with SSID ' + ssid)
-					servicenum=servicenum+1
-					if servicenum > 10: 	#failsave to prevent endless searches
-						reachedend = True
-			DomoticzEx.Status('Fritzbox has ' + str(servicenum-1) + ' WLAN services')
 			self.is_ready = True				
 
 	def stop_now(self):
